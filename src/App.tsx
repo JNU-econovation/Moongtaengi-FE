3947724f1e8b2ce888dfeecee6f6f4d07d6640c6
Kim Jinsoo <jskimcs1@gmail.com>
2025-11-21 11:57:59 +0900

    feat: 회원가입 확인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import KakaoLogin from './pages/KakaoLogin'
 import Signup from './pages/Signup'
+import SignupCheck from './pages/SignupCheck'
 
 function App() {
 
   return (
-    <Signup />
+    <SignupCheck />
   )
 }
 
---
+++ src/pages/SignupCheck.tsx
@@ -0,0 +1,26 @@
+import styles from './SignupCheck.module.css'
+import signupCheck from '../assets/signup-check.png'
+
+const SignupCheck = () => {
+    return (
+        <div className={styles["container"]}>
+            <div className={styles["content-wrapper"]}>
+                <div className={styles["header"]}>
+                    <p className={styles["title"]}>뭉탱이</p>
+                    <p className={styles["subtitle"]}>작은 루틴이<br />큰 성장을 만듭니다</p>
+                </div>
+                <div className={styles["check-group"]}>
+                    <img src={signupCheck} className={styles["check-image"]}></img>
+                    <p className={styles["check-confirm"]}>회원가입이 완료되었습니다.</p>
+                    <p className={styles["check-welcome"]}>
+                        xxxxxx님, 뭉탱이의 회원이 되신 것을 환영합니다.<br/>
+                        이제 뭉탱이와 함께 한 걸음씩 성장해요.
+                    </p>
+                </div>
+                <div className={styles["start-button"]}>회원가입 완료</div>
+            </div>
+        </div>
+    )
+}
+
+export default SignupCheck;
