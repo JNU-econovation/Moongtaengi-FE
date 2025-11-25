a0dc4be8cfd87a84dc72b4c8d77d89fe5ed44094
Kim Jinsoo <jskimcs1@gmail.com>
2025-11-26 02:49:00 +0900

    feat: 토큰 저장과 로그인 여부 확인

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,48 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 const Main = () => {
 
     const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [islogin, setIslogin] = useState<boolean>(false);
+
+    const logout = () => {
+        sessionStorage.removeItem('JWT');
+        setIslogin(false);
+        alert('로그아웃 되었습니다.');
+        navigate('/', {replace: true});
+    }
+
+    useEffect(() => {
+        let jwt: string | null = sessionStorage.getItem('JWT');
+
+        if (jwt) {
+            setIslogin(true);
+            alert('로그인 되었습니다.');
+        }
+    }, [])
+
+    useEffect(() => {
+        const token: string | null = searchParams.get('token');
+
+        if (token) {
+            sessionStorage.setItem('JWT', token);
+            setIslogin(true);
+            alert('회원가입 완료 후 토큰 저장 되었습니다.');
+
+            navigate('/', {replace: true});
+        }
+    }, [searchParams]);
 
     return (
         <>
-            <p style={{color: "white"}}>메인 화면임</p>
-            <button onClick={() => {navigate('/login')}}>로그인 하기</button>
+            <p style={{ color: "white" }}>메인 화면임</p>
+            {
+                islogin
+                    ? <button onClick={() => { logout() }}>로그아웃 하기</button>
+                    : <button onClick={() => { navigate('/login') }}>로그인 하기</button>
+            }
         </>
     )
 }
