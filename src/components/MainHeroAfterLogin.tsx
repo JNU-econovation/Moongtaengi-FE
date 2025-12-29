2448bd2100ad3174f34e053b186ba073f9e91d88
Kim Jinsoo <jskimcs1@gmail.com>
2025-12-29 21:01:51 +0900

    refactor: 메인 페이지 이미지 관련 수정

--- src/components/MainHeroAfterLogin.tsx
+++ src/components/MainHeroAfterLogin.tsx
@@ -1,7 +1,7 @@
 import mainBg from "../assets/main/main-bg.png";
 import mainMoongs from "../assets/main/main-moongs.png";
 
-import mainMoongCompB from "../assets/main/main-moong-comp-a.png";
+import mainMoongCompB from "../assets/main/main-moong-comp-b.png";
 import mainProfile from "../assets/main/main-profile.png";
 import MainButton from "./MainButton";
 
@@ -16,7 +16,7 @@ export default function MainHeroAfterLogin({ islogin, isStudy }: HeroProps) {
         <div className="relative w-full flex flex-col md:flex-row md:h-[500px] 2xl:h-[600px]">
 
             {/* Left: Text Content */}
-            <div className="flex-1 flex flex-col justify-center ml-16 z-10 text-white">
+            <div className="flex-1 flex flex-col justify-center md:ml-20 2xl:ml-34 z-10 text-white md:scale-100 2xl:scale-110">
                 <span className="font-semibold text-lg">xxxxxxx 님, 안녕하세요.</span>
                 <h2 className="md:text-6xl 2xl:text-7xl font-semibold leading-tight mb-8">
                     뭉탱이 뭉치
@@ -49,9 +49,9 @@ export default function MainHeroAfterLogin({ islogin, isStudy }: HeroProps) {
             </div>
 
             {/* Right: Character Area */}
-            <div className="absolute inset-0 md:static md:flex-1">
-                <div className="w-full h-full relative flex items-center justify-center">
-                    <img src={mainMoongCompB} className="absolute w-full h-full mr-40 "></img>
+            <div className="absolute inset-0 static flex-1">
+                <div className="relative md:w-[110%] 2xl:w-full h-full flex items-center justify-center">
+                    <img src={mainMoongCompB} className="absolute w-full h-full mr-60"></img>
 
                     {/* <img src={mainBg} className="absolute w-full h-full mr-40 object-cover"></img>
                     <img src={mainMoongs} className="absolute md:right-48 2xl:right-70 z-10 md:w-100 2xl:w-130 md:h-100 2xl:h-130 flex items-center justify-center"></img>
