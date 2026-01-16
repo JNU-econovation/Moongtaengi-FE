db69881d9e334c90dcbb9f839bc89add22494b3d
Kim Jinsoo <jskimcs1@gmail.com>
2026-01-16 17:31:27 +0900

    feat: 과제 댓글 창 추가

--- src/pages/AssignmentEdit.tsx
+++ src/pages/AssignmentEdit.tsx
@@ -22,10 +22,17 @@ export const AssignmentEdit = () => {
 
     const [_, forceUpdate] = useState(0);
 
+    const [commentOpen, setCommentOpen] = useState(false);
+    const [commentInupt, setCommentInput] = useState('');
+
+    const [fileName, setFileName] = useState('');
+
+    // 파일 임베딩을 요청하기 위한 ref
     const imageInputRef = useRef<HTMLInputElement>(null);
     const fileInputRef = useRef<HTMLInputElement>(null);
 
-    const [fileName, setFileName] = useState('');
+    // 댓글 입력 박스 높이 조절을 위한 ref
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
     const editor = useEditor({
         extensions: [
@@ -92,7 +99,7 @@ export const AssignmentEdit = () => {
         }
     }
 
-    const handleFileSelect = async (e:React.ChangeEvent<HTMLInputElement>) => {
+    const handleFileSelect = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
         if (!file) return;
 
@@ -109,6 +116,14 @@ export const AssignmentEdit = () => {
         }
     }
 
+    // 댓글 입력 박스 높이 조절 핸들
+    const handleResizeHeight = () => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = 'auto'; // 높이 초기화
+            textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px' // 높이 조절
+        }
+    }
+
     // const handleLog = () => {
     //     console.log(editor.storage.markdown.getMarkdown());
     // }
@@ -119,7 +134,7 @@ export const AssignmentEdit = () => {
     const activeClass = "bg-white py-1 rounded";
 
     return (
-        <div className="flex-1 flex flex-col">
+        <div className="h-full flex flex-col relative">
             <input
                 type='file'
                 accept='image/*'
@@ -136,14 +151,16 @@ export const AssignmentEdit = () => {
             />
 
             {/* 뒤로가기, 댓글 */}
-            <div className='relative w-full h-24 flex items-center md:mb-2 2xl:mb-10'>
+            <div className='relative w-full h-24 flex items-center md:mb-2 2xl:mb-10 z-50'>
                 <button
                     onClick={() => { navigate(`/studies/${studyId}/processes/${processId}/assignments/${assignmentId}`) }}
                     className="absolute left-8 w-10 h-10 rounded-full bg-[#272727] flex items-center justify-center hover:opacity-70 cursor-pointer"
                 >
                     <img src={downArrow} className='invert rotate-90 w-[40%]' />
                 </button>
-                <button className="absolute right-8 w-10 h-10 rounded-full bg-[#2a2a2a] flex items-center justify-center hover:opacity-70 cursor-pointer">
+                <button
+                    onClick={() => { setCommentOpen(!commentOpen) }}
+                    className={`absolute right-8 w-10 h-10 rounded-full ${commentOpen ? 'bg-white' : 'bg-[#2a2a2a]'} flex items-center justify-center hover:opacity-70 cursor-pointer`}>
                     <img src={hamburgerBar} className='w-[40%]' />
                 </button>
             </div>
@@ -249,7 +266,7 @@ export const AssignmentEdit = () => {
                         {/* 파일 임베드 */}
                         <div className='flex-1'>
                             <button
-                                onClick={() => {fileInputRef.current?.click()}}
+                                onClick={() => { fileInputRef.current?.click() }}
                                 className="text-[#5F5F5F] text-sm flex items-center cursor-pointer hover:opacity-70"
                             >
                                 <img src={fileIcon} className='w-[8%] mr-1' />
@@ -261,13 +278,69 @@ export const AssignmentEdit = () => {
 
                         {/* 등록 버튼 */}
                         <button
-                            
                             className="bg-white text-[#6D6D6D] px-4 py-1.5 rounded text-sm font-semibold hover:text-black transition-colors cursor-pointer">
                             등록
                         </button>
                     </div>
                 </div>
             </div>
+
+            {commentOpen && (
+                <aside className='absolute top-0 right-0 md:w-60 2xl:w-90 h-full bg-[#272727] flex flex-col gap-2'>
+                    <header className='p-6 pt-8'>
+                        <div className='flex flex-col gap-1'>
+                            <p className='text-3xl'>UI 개설</p>
+                            <p>매코미통닭발</p>
+                        </div>
+                    </header>
+
+                    <main className='flex-1 flex flex-col gap-2 p-6'>
+                        <p className='text-white text-sm'>2개의 댓글들</p>
+                        <div className={`flex-1 overflow-y-auto max-h-133 pr-1 flex flex-col items-center gap-2 
+                                [&::-webkit-scrollbar]:w-1
+                                [&::-webkit-scrollbar-track]:bg-transparent
+                                [&::-webkit-scrollbar-thumb]:bg-[#555]
+                                [&::-webkit-scrollbar-thumb]:rounded-full
+                                [&::-webkit-scrollbar-button]:hidden `}
+                        >
+                            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((comment, index) => (
+                                <div className='w-full p-2 bg-[#393939] rounded'>
+                                    <div className='flex'>
+                                        <div className='shrink-0 border w-10 h-10 rounded-full'>
+                                            아이콘
+                                        </div>
+                                        <div className='flex-1 bg-black'>
+                                            dfasfasdfadsf
+                                        </div>
+                                        <div className='shrink-0'>
+                                            ❤️
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </main>
+
+                    <footer className='bg-[#393939]'>
+                        <div className='relative flex items-end'>
+                            <textarea
+                                ref={textareaRef}
+                                rows={1}
+                                value={commentInupt}
+                                onChange={(e) => {
+                                    setCommentInput(e.target.value);
+                                    handleResizeHeight();
+                                }}
+                                placeholder='여기에 내용을 입력하세요.'
+                                className='w-[94%] max-h-27 min-h-10 resize-none bg-white rounded-[20px] text-black px-4 pr-10 py-2 placeholder:text-black mt-2 ml-3 mb-4 overflow-hidden'
+                            />
+                            <button className='absolute bottom-6 right-6 text-black cursor-pointer'>
+                                d^
+                            </button>
+                        </div>
+                    </footer>
+                </aside>
+            )}
         </div>
     );
 };
