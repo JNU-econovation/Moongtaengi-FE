8bab6e9f2fc1afe15d092ffc0ea99456747514f3
Kim Jinsoo <jskimcs1@gmail.com>
2026-01-22 19:46:00 +0900

    feat: 메인 페이지 api

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,24 +1,25 @@
-import { useNavigate, type NavigateFunction } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useModalModeStore } from "../stores/useModalModeStore";
 
 interface HeroProps {
     islogin: boolean;
-    isStudy: boolean;
+    shortcutStudyId?: number | null;
+    totalExperience?: number;
 }
 
-export default function MainButton({ islogin, isStudy }: HeroProps) {
+export default function MainButton({ islogin, shortcutStudyId, totalExperience }: HeroProps) {
+
+    if (!islogin) return <LoginButton />
+
+    if (!shortcutStudyId) return <CreateStudyButton  />
+
+    return <ProgressButton totalExperience={totalExperience} />
+}
+
+
+const LoginButton = () => {
     const navigate = useNavigate();
 
-    if (!islogin) return <LoginButton navigate={navigate} />
-    if (!isStudy) return <CreateStudyButton navigate={navigate} />
-    return <ProgressButton navigate={navigate} progress={20} />
-}
-
-
-interface ButtonProps {
-    navigate: NavigateFunction
-}
-
-const LoginButton = ({ navigate }: ButtonProps) => {
     return (
         <button className="w-80 px-10 md:py-3 2xl:py-4 text-2xl font-semibold text-black bg-gradient-to-b from-custom-gradient-blue to-custom-gradient-green rounded-full hover:opacity-70 transition md:border-1 2xl:border-2 border-white cursor-pointer"
             onClick={() => { navigate('/login') }}>
@@ -27,29 +28,38 @@ const LoginButton = ({ navigate }: ButtonProps) => {
     )
 }
 
-const CreateStudyButton = ({ navigate }: ButtonProps) => {
+const CreateStudyButton = () => {
+    const setModalMode = useModalModeStore((s) => s.setModalMode);
+
     return (
-        <button className="w-80 px-10 md:py-3 2xl:py-4 text-2xl font-semibold text-white bg-gradient-to-b from-custom-gradient-blue to-custom-gradient-green rounded-full hover:opacity-70 transition md:border-1 2xl:border-2 border-white cursor-pointer"
-            onClick={() => { navigate('/createStudy') }}>
+        <button
+            onClick={() => { setModalMode('createStudy') }}
+            className="w-80 px-10 md:py-3 2xl:py-4 text-2xl font-semibold text-white bg-gradient-to-b from-custom-gradient-blue to-custom-gradient-green rounded-full hover:opacity-70 transition md:border-1 2xl:border-2 border-white cursor-pointer"
+        >
             스터디 생성하기
         </button>
     )
 }
 
-interface ProgressButton extends ButtonProps {
-    progress: number;
+interface ProgressButton {
+    totalExperience: number;
 }
 
-const ProgressButton = ({ navigate, progress }: ProgressButton) => {
+const ProgressButton = ({ totalExperience }: ProgressButton) => {
+    const navigate = useNavigate();
+    const progress = totalExperience / 300 * 100;
+
     return (
-        <button className="relative w-80 rounded-full md:border-1 2xl:border-2 border-white cursor-pointer overflow-hidden bg-custom-bg hover:opacity-70 transition group"
-            onClick={() => { navigate('/currentProject') }}>
+        <button
+            onClick={() => { navigate('/currentProject') }}
+            className="relative w-80 rounded-full md:border-1 2xl:border-2 border-white cursor-pointer overflow-hidden bg-custom-bg hover:opacity-70 transition group"
+        >
             <div
                 className="absolute top-0 left-0 h-full bg-gradient-to-b from-custom-gradient-blue to-custom-gradient-green transition-all duration-500 ease-out"
                 style={{ width: `${progress}%` }}
-            ></div>
+            />
             <span className="relative z-10 w-full h-full flex items-center justify-center px-10 md:py-3 2xl:py-4 text-2xl font-semibold text-white">
-                프로젝트 바로가기
+                스터디 바로가기
             </span>
         </button>
     )
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import MainBottom from "../components/MainBottom";
 import MainHeroAfterLogin from "../components/MainHeroAfterLogin";
 import MainHeroBeforeLogin from "../components/MainHeroBeforeLogin";
@@ -7,13 +6,12 @@ import { useAuthStore } from "../stores/useAuthStore";
 const Main = () => {
 
     const { isLogin } = useAuthStore();
-    const [isStudy] = useState<boolean>(true);
 
     return (
         <>
             {isLogin
-                ? <MainHeroAfterLogin isLogin={isLogin} isStudy={isStudy} />
-                : <MainHeroBeforeLogin isLogin={isLogin} isStudy={isStudy} />}
+                ? <MainHeroAfterLogin isLogin={isLogin} />
+                : <MainHeroBeforeLogin isLogin={isLogin} />}
             <MainBottom />
 
         </>
